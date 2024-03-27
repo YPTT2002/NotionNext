3cf675571aeff0b02e759de561d7b73ddb8ca553
YPTT2002 <165188536+YPTT2002@users.noreply.github.com>
2024-03-28 01:12:51 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -22,7 +22,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '放飞机会计师事务所', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '放飞机会计师事务所深圳总所（特殊普通散伙）', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-psi-navy.vercel.app/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-my9qloydw-ypts-projects.vercel.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
